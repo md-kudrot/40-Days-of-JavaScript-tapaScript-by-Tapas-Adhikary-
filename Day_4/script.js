5e93de8f0451c574dda1595f5e6f19df18a9e54c
md-kudrot-592 <kk.mdkudrot@gmail.com>
2025-10-27 23:02:09 +0600

    Day 4 practice done

---
+++ Day_4/script.js
@@ -0,0 +1,29 @@
+// console.log("hello")
+
+// switch and case
+
+let value = 5
+
+switch(value){
+    case 1 :
+        console.log("this is case one");
+        break;
+    case 2 :
+        console.log("this is case 2");
+        break;
+    case 3 :
+        console.log("this is case 3");
+        break;
+    case 4 :
+        console.log("this is case 4");
+        break;
+    case 5 :
+        console.log("this is case 5");
+        break;
+    case 6 :
+        console.log("this is case 6");
+        break;
+    
+    default:
+        console.log("this is defaul")
+}
