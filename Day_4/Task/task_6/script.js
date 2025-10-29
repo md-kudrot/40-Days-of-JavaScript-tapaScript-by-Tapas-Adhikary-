67921e4065250a8ac2c605c05bb362175a7f5b66
md-kudrot-592 <kk.mdkudrot@gmail.com>
2025-10-29 16:14:27 +0600

    day 4 done

--- Day_4/Task/task_6/script.js
+++ Day_4/Task/task_6/script.js
@@ -1,4 +1,21 @@
-let sideOne = 1;
-let sideTwo = 0;
-let sideThree = 1;
+let sideOne = 5;
+let sideTwo = 5;
+let sideThree = 2;
+
+if(sideOne + sideTwo > sideThree && sideTwo + sideThree > sideOne && sideThree + sideOne > sideTwo ){
+    console.log("triangle")
+}else{
+    console.log("Not a triangle")
+    return;
+}
+
+if(sideOne == sideTwo && sideTwo == sideThree){
+    console.log("Equilateral")
+}else if(sideOne == sideTwo || sideTwo == sideThree || sideOne == sideThree){
+    console.log("Isosceles")
+}else{
+    console.log("Scalene")
+}
+
+
 
