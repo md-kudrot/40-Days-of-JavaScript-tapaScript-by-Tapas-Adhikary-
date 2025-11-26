a7428c0582853df879f4575a5c555999b6cefdf0
md-kudrot-592 <kk.mdkudrot@gmail.com>
2025-11-26 20:25:16 +0600

    function , Loop Destructuring and  Optional Chaining

--- Day_12/objectDestructuring.js
+++ Day_12/objectDestructuring.js
@@ -1,4 +1,64 @@
 const student = {
+  'name': 'John Williamson',
+  'age': 9,
+  'std': 3,
+  'subjects': ['Maths', 'English', 'EVS'],
+  'parents': {
+    'father': 'Brown Williamson',
+    'mother': 'Sophia',
+    'email': 'john-parents@abcde.com'
+  },
+  'address': {
+    'street': '65/2, brooklyn road',
+    'city': 'Carterton',
+    'country': 'New Zealand',
+    'zip': 5791
+  }
+}
+
+// *****************
+//Destructuring = short code
+const nam = student.name
+// we can write this in shortly
+const { name } = student
+console.log(name)
+
+// ********************
+// use can access multiple property
+// const {std, age} = student
+// console.log(std,age)
+
+// ******************
+// we can also assign new property in this way
+const { meal = "milk" } = student;
+
+console.log(meal)
+
+// we can rename any property which is avaiable in object
+
+const { age: boyos } = student
+
+// console.log(boyos)
+
+// we can access the nasted object property in this way
+
+const { address: { zip } } = student;
+// console.log(zip)
+
+
+// function Destructuring
+
+function sendEmail({ parents: { email } }) {
+  // console.log(`Sent an email to ${email}`)
+}
+
+sendEmail(student)
+
+
+
+
+const getStudent = () => {
+  return {
     'name': 'John Williamson',
     'age': 9,
     'std': 3,
@@ -14,33 +74,68 @@ const student = {
       'country': 'New Zealand',
       'zip': 5791
     }
-   }
+  }
+}
 
-// *****************
-//Destructuring = short code
-const nam = student.name
-// we can write this in shortly
-const {name} = student
-console.log(name)
+// General way to access name and subjects
 
-// ********************
-// use can access multiple property
-// const {std, age} = student
-// console.log(std,age)
+// const anotherStu = getStudent()
+// const anotherStuName = anotherStu.name
+// const anotherStuSub = anotherStu.subjects
 
-// ******************
-// we can also assign new property in this way
-const {meal = "milk" } = student ;
+// console.log(anotherStuName , anotherStuSub)
 
-console.log(meal)
+// we can do this same work in shortcut using Destructuring
 
-// we can rename any property which is avaiable in object
+const {name: anotherStuName , subjects: anotherStuSub} = getStudent();
 
-const {age: boyos} = student
+// console.log(anotherStuName,anotherStuSub)
 
-console.log(boyos)
 
-// we can access the nasted object property in this way
 
-const {address:{zip}} = student;
-console.log(zip)
+
+
+
+
+
+
+// Loop Destructuring
+
+const students = [
+  {
+    'name': 'William',
+    'grade': 'A'
+  },
+  {
+    'name': 'Tom',
+    'grade': 'A+'
+  },
+  {
+    'name': 'Bob',
+    'grade': 'B'
+  }
+];
+
+for (let {name,grade} of students){
+  // console.log(name,grade)
+}
+
+
+// Optional Chaining
+
+const employee = {
+  slary: {
+    bonus: 300
+  }
+}
+
+// when we try to access any property which is not available in object , we get error / undefined
+// example:
+// console.log(employee.depertment): // undefined
+// console.log(employee.department.name); //error
+// ei error theke basar jonno short way ase
+
+// short way to pass the error and get undefined
+const empDep = employee.department?.name 
+
+console.log(empDep) // undefined
