c11a0ee2c68e9f96f95a524e5661aeb302ce713f
md-kudrot-592 <kk.mdkudrot@gmail.com>
2025-11-30 19:30:55 +0600

    Day 12 done , task 9,10 done

---
+++ Day_12/Task/task10.js
@@ -0,0 +1,25 @@
+// 10. Loop and print values using Object destructuiring
+
+
+const users = [
+  {
+      'name': 'Alex',
+      'address': '15th Park Avenue',
+      'age': 43
+  },
+  {
+      'name': 'Bob',
+      'address': 'Canada',
+      'age': 53
+  },
+  {
+      'name': 'Carl',
+      'address': 'Bangalore',
+      'age': 26
+  }
+];
+
+for ({name,address,age} of users){
+    console.log(name,address,age)
+}
+
--- Day_12/Task/task9.js
+++ Day_12/Task/task9.js
@@ -1,2 +1,22 @@
 // 9. What’s the best way to deeply copy a nested object? Expalin with examples
 
+// answer:
+// structuredClone() is  the best way to deeply copy a nested Object
+
+const obj1 = {
+    name : "kk",
+    address: {
+        city: "Dhaka"
+    }
+}
+ 
+const obj2 = structuredClone(obj1); //Deep Copy
+
+// const obj2 = Object.assign(obj1); // Shallow Copy
+
+console.log(obj2)
+
+obj1.address.city = "Dinajpur";
+
+console.log(obj1.address.city)
+console.log(obj2.address.city)
